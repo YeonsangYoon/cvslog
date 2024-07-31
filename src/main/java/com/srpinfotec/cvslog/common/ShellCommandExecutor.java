1911f1754061591ba00e71e3686d512a09bdf2c3
YeonsangYoon <1004dustkd@gmail.com>
2024-07-31 16:34:24 +0900

    debug log

--- src/main/java/com/srpinfotec/cvslog/common/ShellCommandExecutor.java
+++ src/main/java/com/srpinfotec/cvslog/common/ShellCommandExecutor.java
@@ -3,6 +3,7 @@ package com.srpinfotec.cvslog.common;
 import com.srpinfotec.cvslog.error.CustomException;
 import com.srpinfotec.cvslog.error.ShellCommandException;
 import com.srpinfotec.cvslog.util.SystemUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 public class ShellCommandExecutor {
 
     // TODO 프로세스 실행 Timeout 기능 추가
@@ -40,6 +42,7 @@ public class ShellCommandExecutor {
             String line;
             while ((line = reader.readLine()) != null) {
                 readLines.add(line);
+                log.debug(line);
             }
             process.waitFor();
 

77eb8fc6cb77eb4e623ea17b8edbc741c7c24d91
YeonsangYoon <1004dustkd@gmail.com>
2024-08-04 18:27:44 +0900

    Log 형식 오류 시 Skip 정책 적용

--- src/main/java/com/srpinfotec/cvslog/batch/RevisionLogFileToDB.java
+++ src/main/java/com/srpinfotec/cvslog/batch/RevisionLogFileToDB.java
@@ -13,21 +13,26 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.transform.DefaultFieldSet;
 import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.batch.item.support.CompositeItemProcessor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 @Slf4j
@@ -51,6 +56,7 @@ public class RevisionLogFileToDB {
                                      ItemReader<RevisionLogEntry> revisionLogItemReader,
                                      ItemProcessor<RevisionLogEntry, Revision> revisionLogItemProcessor,
                                      ItemWriter<Revision> revisionItemWriter,
+                                     SkipPolicy parseLogSkipPolicy,
                                      @Value("#{jobParameters['chuckSize']}") Long chuckSize
     ){
         return new StepBuilder("RevisionFileToDBStep", jr)
@@ -58,6 +64,8 @@ public class RevisionLogFileToDB {
                 .reader(revisionLogItemReader)
                 .processor(revisionLogItemProcessor)
                 .writer(revisionItemWriter)
+                .faultTolerant()
+                .skipPolicy(parseLogSkipPolicy)
                 .build();
     }
 
@@ -85,16 +93,38 @@ public class RevisionLogFileToDB {
 
     @Bean
     @StepScope
-    public ItemProcessor<RevisionLogEntry, Revision> revisionLogItemProcessor(){
+    public CompositeItemProcessor<RevisionLogEntry, Revision> revisionLogItemProcessor(){
+        CompositeItemProcessor<RevisionLogEntry, Revision> compositeItemProcessor = new CompositeItemProcessor<>();
+
+        compositeItemProcessor.setDelegates(Arrays.asList(
+                duplicationCheckItemProcessor(),
+                dtoToEntityItemProcessor()
+        ));
+
+        return compositeItemProcessor;
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<RevisionLogEntry, RevisionLogEntry> duplicationCheckItemProcessor(){
+        return new ItemProcessor<RevisionLogEntry, RevisionLogEntry>() {
+            @Override
+            public RevisionLogEntry process(RevisionLogEntry logEntry) throws Exception {
+                if(logEntry == null) return null;
+
+                Optional<Revision> byLog = revisionRepository.findByLog(logEntry.getFilename(), logEntry.getFilepath(), logEntry.getVersion());
+
+                return byLog.isEmpty() ? logEntry : null;
+            }
+        };
+    }
+
+    @Bean
+    @StepScope
+    public ItemProcessor<RevisionLogEntry, Revision> dtoToEntityItemProcessor(){
         return new ItemProcessor<RevisionLogEntry, Revision>() {
             @Override
             public Revision process(RevisionLogEntry logEntry) throws Exception {
-                Optional<Revision> byLog = revisionRepository.findByLog(logEntry.getFilename(), logEntry.getFilepath(), logEntry.getVersion());
-
-                if(byLog.isPresent()){
-                    return null;
-                }
-
                 Project project = projectRepository.findByNaturalId(logEntry.getProjectName())
                         .orElseGet(() -> new Project(logEntry.getProjectName()));
 
@@ -130,6 +160,16 @@ public class RevisionLogFileToDB {
             public void write(Chunk<? extends Revision> chunk) throws Exception {
                 chunk.forEach(revisionRepository::save);
                 log.debug("Write revision log to database (revision count : {})", String.valueOf(chunk.size()));
+            }
+        };
+    }
+
+    @Bean
+    public SkipPolicy parseLogSkipPolicy(){
+        return new SkipPolicy() {
+            @Override
+            public boolean shouldSkip(Throwable t, long skipCount) throws SkipLimitExceededException {
+                return t instanceof FlatFileParseException && skipCount <= 3;
             }
         };
     }
--- src/main/java/com/srpinfotec/cvslog/batch/mapper/LogToEntityMapper.java
+++ src/main/java/com/srpinfotec/cvslog/batch/mapper/LogToEntityMapper.java
@@ -2,34 +2,46 @@ package com.srpinfotec.cvslog.batch.mapper;
 
 import com.srpinfotec.cvslog.domain.RevisionType;
 import com.srpinfotec.cvslog.dto.RevisionLogEntry;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.validation.BindException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 /**
  * Revision Log를 엔티티로 변환해주는 Mapper 클래스
  */
 public class LogToEntityMapper implements FieldSetMapper<RevisionLogEntry> {
+    private static final Integer EXPECTED_FIELD_COUNT = 10;
+
     @Override
     public RevisionLogEntry mapFieldSet(FieldSet fieldSet) throws BindException {
-        LocalDateTime date = LocalDateTime.parse(
-                fieldSet.readString(1) + " " + fieldSet.readString(2),
-                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-        );
+        if(fieldSet.getFieldCount() != EXPECTED_FIELD_COUNT){
+            throw new FlatFileParseException("로그 형식 오류", Arrays.toString(fieldSet.getValues()));
+        }
 
-        String[] path = fieldSet.readString(7).split("/");
+        try{
+            LocalDateTime date = LocalDateTime.parse(
+                    fieldSet.readString(1) + " " + fieldSet.readString(2),
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+            );
 
-        return new RevisionLogEntry(
-                RevisionType.getType(fieldSet.readString(0)),
-                fieldSet.readString(6),
-                fieldSet.readString(7),
-                fieldSet.readString(4),
-                Long.parseLong(fieldSet.readString(5).substring(2)),
-                date,
-                path[0]
-        );
+            String[] path = fieldSet.readString(7).split("/");
+
+            return new RevisionLogEntry(
+                    RevisionType.getType(fieldSet.readString(0)),
+                    fieldSet.readString(6),
+                    fieldSet.readString(7),
+                    fieldSet.readString(4),
+                    Long.parseLong(fieldSet.readString(5).substring(2)),
+                    date,
+                    path[0]
+            );
+        } catch (Exception e){
+            throw new FlatFileParseException("로그 형식 오류", Arrays.toString(fieldSet.getValues()));
+        }
     }
 }

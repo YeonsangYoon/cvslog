204b238ccff2785df56544360b3b6b6bf382c85c
YeonsangYoon <1004dustkd@gmail.com>
2024-11-01 11:11:12 +0900

    deploy script restart 로직 추가

--- run.sh
+++ run.sh
@@ -17,30 +17,45 @@ JAR_FILE=cvslog-0.0.1.jar
 JAVA_OPT="-Xmx256m -Xmx256m -Dspring.profiles.active=prod "$JAVA_OPT
 
 ## 4. 프로그램 실행
+start() {
+        echo "start application"
+        nohup java $JAVA_OPT -jar $JAR_FILE > /dev/null 2> /dev/null &
+
+        # PID 저장
+        echo $! > ./application.pid
+        echo "applcation started"
+}
+
+stop() {
+        # 프로그램이 실행중이 아니면 종료
+        if [ -f ./application.pid ]; then
+                echo "stop application"
+
+                # Process ID 식별
+                PID=`cat ./application.pid`
+                echo "application is running as $PID"
+
+                # 종료
+                kill -15 $PID
+                echo "application stopped"
+
+                # pid 파일 삭제
+                rm ./application.pid
+        else
+                echo "application not running"
+        fi
+}
+
 case $1 in
         start)
-                echo "start application"
-                nohup java $JAVA_OPT -jar $JAR_FILE > /dev/null 2> /dev/null &
-
-                # PID 저장
-                echo $! > ./application.pid
-                echo "application started"
-
+                start
         ;;
         stop)
-                # 프로그램이 실행중이 아니면 종료
-                if [ -f ./application.pid ]; then
-                        echo "stop application"
-                        # Process ID 식별
-                        PID=`cat ./application.pid`
-                        echo "application is running as $PID"
-                        # 종료
-                        kill -15 $PID
-                        echo "application stopped"
-                        # pid 파일 삭제
-                        rm ./application.pid
-                else
-                        echo "application not running"
-                fi
+                stop
+        ;;
+        restart)
+                stop
+                sleep 10
+                start
         ;;
 esac
